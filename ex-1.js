3ef25182b81442f6c77bb3e2296ea402d306853d
BP13 Champ <monkzaa007@hotmail.com>
2026-06-11 01:24:26 +0700

    finish:Debug

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
+accountBalance = Number(accountBalance);
+depositAmountFromUser = Number(depositAmountFromUser);
 
-accountBalance = accountBalance + depositAmountFromUser;
+accountBalance = accountBalance + depositAmountFromUser; 
 
-let accountBalanceMessage = "Your account balance is " + accountBalance;
+let accountBalanceMessage = `Your account balance is ${accountBalance}`; 
 
-console.log(accountBalanceMessage);
+console.log(accountBalanceMessage); 
